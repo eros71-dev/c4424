a5fa79dad949f8006c3c5738f57f0dd28c385d43
SonicDarkYT <99994913+SonicDarkYT@users.noreply.github.com>
2024-02-21 18:14:44 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,23 +1,58 @@
-emblems = {"luigi_texture_l_cap", "mario_texture_m_cap", "mario_texture_m_logo", "mario_texture_m_blend", "toad_player_texture_spots", "toad_player_texture_cap"}
-emblemswl = {"waluigi_custom_waluigi_cap_rgba16", "waluigi_custom_waluigi_glove_rgba16", "wario_texture_w_logo"}
+-- name: C4424
+-- description: idk yet
+-- deluxe: true
 
-function no_emblems(msg)
-    local no_emblems = get_texture_info("no_emblems")
-    local no_emblemswl = get_texture_info("no_emblemswl")
-    for i = 1, #emblems do
-        if msg == "off" then
-            texture_override_set(emblems[i], no_emblems)
-        elseif msg == "on" then
-            texture_override_reset(emblems[i])
-        end
-    end
-    for i = 1, #emblemswl do
-        if msg == "off" then
-            texture_override_set(emblemswl[i], no_emblemswl)
-        elseif msg == "on" then
-            texture_override_reset(emblemswl[i])
-        end
-    end
+--[[
+
+Possible features, name indicates who suggested it, not who implemented it:
+
+    Agent X
+unregistered hypercam // DONE
+G_TF_BILERP (texture filtered) HUD
+stretched viewport? ("widescreen")
+shadow textures that are just completely empty
+pitched up sounds
+low framerate (shown in video, so i'll leave it here in case he wants to add it)
+
+    Citra
+missing textures
+fucked up vertices
+
+    eros71
+broken animations
+(bandicam variant for watermark?) // DONE
+z-fighting
+
+    SonicDark
+texture corruption / bleeding
+no M / emblems sometimes
+
+
+
+(If you want to credit yourself for the stuff you've added, you can do so below like this)
+Dev:
+Feature1
+Feature2
+    <empty line>
+
+----------------
+
+eros71:
+Watermarks
+Classic mode
+
+(Next dev here)
+]]--
+
+if not USING_COOPDX then djui_popup_create_global("This mod requires SM64Coop DX to work properly.", 2) end
+-- afaik things like lua texture swapping require it, not sure if this line has to be in each lua file or just main -eros71
+
+
+local function on_reset_mod_save_command()
+    mod_storage_remove("firstRunDone")
+    djui_popup_create("You may need to rehost in order to see the changes.", 2)
+    return true
 end
 
-hook_chat_command('emblems', "[on|off] Disable the character emblems like in the old machinimas", no_emblems)
+hook_chat_command("reset_c4424_data", "- This command will ERASE ALL c4424 data! Use with caution.",
+    on_reset_mod_save_command)
