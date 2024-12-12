02b1b69023f4a2e2c11746737bdd92e90dc221af
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2024-12-12 03:25:20 +0100

    Actually use that table, sorry for the push :(

--- c-music.lua
+++ c-music.lua
@@ -1,4 +1,4 @@
-_G.c4424.sMusicTable = {
+_G.c4424._G.c4424.sMusicTable = {
     { stream = audio_stream_load("trance.mp3"),         volume = 0.8, displayName = "Trance - 009 Sound System" },
     { stream = audio_stream_load("schemingweasel.mp3"), volume = 1.2, displayName = "Scheming Weasel - Kevin MacLeod" },
     { stream = audio_stream_load("monkeys.mp3"),        volume = 1.1, displayName = "Monkeys Spinning Monkeys - Kevin MacLeod" },
@@ -11,7 +11,7 @@ local currentMusic = 0
 function handle_classic_music()
     if not playMusic or not c4424Enabled then
         if currentMusic ~= 0 then
-            audio_stream_stop(sMusicTable[currentMusic].stream)
+            audio_stream_stop(_G.c4424.sMusicTable[currentMusic].stream)
             currentMusic = 0
         end
         set_background_music(SEQ_PLAYER_LEVEL, levelSeq, 0)
@@ -21,15 +21,15 @@ function handle_classic_music()
     -- 25% chance of playing the music
     if math.random(1, 100) > 25 then
         local newMusic = if_then_else(math.random(1, 100) <= 50 and currentMusic ~= 0, currentMusic,
-            math.random(1, #sMusicTable))                                                                                          -- 50% chance to just be the same music
+            math.random(1, #_G.c4424.sMusicTable))                                                                                          -- 50% chance to just be the same music
         if newMusic == currentMusic then
             set_background_music(SEQ_PLAYER_LEVEL, 0, 0)
             return
         end
 
-        if currentMusic ~= 0 then audio_stream_stop(sMusicTable[currentMusic].stream) end
+        if currentMusic ~= 0 then audio_stream_stop(_G.c4424.sMusicTable[currentMusic].stream) end
 
-        local music = sMusicTable[newMusic]
+        local music = _G.c4424.sMusicTable[newMusic]
         currentMusic = newMusic
         audio_stream_play(music.stream, true, music.volume)
         audio_stream_set_looping(music.stream, true)
